00cadcf287ed567e73f04edcdf32d925975b8973
r-firdovsi <r-firdovsi@hotmail.com>
2019-05-31 21:06:21 +0400

    Comming Soon Smooth Scrolling and Full Responsibilty

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+var map;
+function initMap() {
+        map = new google.maps.Map(document.querySelector('.map'), {
+          center: {lat: 40.391944, lng: 49.847539},
+          zoom: 8
+ });
+
+}
